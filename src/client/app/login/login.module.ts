dca8f821f3d381028e84c96aed2e4612b754179f
NickBolles <nickbolles@live.com>
2017-02-08 16:04:15 -0600

    Materialize login component

--- src/client/app/login/login.module.ts
+++ src/client/app/login/login.module.ts
@@ -5,12 +5,15 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 
+import { MaterialModule } from "@angular/material";
+
 @NgModule({
   imports: [
     LoginRoutingModule,
     CommonModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    MaterialModule.forRoot()
   ],
   declarations: [
     LoginComponent
