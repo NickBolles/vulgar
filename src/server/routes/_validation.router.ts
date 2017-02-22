6879a3f122e4e38941b8b425ea987be55c63b4a1
NickBolles <nickbolles@live.com>
2017-02-21 20:41:28 -0600

    add an email validator

--- src/server/routes/_validation.router.ts
+++ src/server/routes/_validation.router.ts
@@ -34,15 +34,37 @@ module Route {
               // If no user was found with a matching username
               if(user === null) {
                 // Set a `HTTP` status code of `404` `Not Found`
-                res.status(404);
                 // Send our validation object
-                res.json({ usernameTaken: false });
+                res.status(404).json({ usernameTaken: false });
               // If a user was found with a matchin username
               } else {
                 // Set a `HTTP` status code of `409` `Conflict`
-                res.status(409);
                 // Send our validation object
-                res.json({ usernameTaken: true });
+                res.status(409).json({ usernameTaken: true });
+              }
+            }
+          });
+        });
+      router.route(`${BASE_URI}/email/:email`)
+        .get((req: express.Request,
+              res: express.Response,
+              next: express.NextFunction) => {
+          let email = { 'local.email': req.params.email };
+          // Use `mongoose` to a single `user` item by `username` in the database
+          Users.findOne(email, (err: any, user: User) => {
+            if(err)
+              res.send(err);
+            else {
+              // If no user was found with a matching username
+              if(user === null) {
+                // Set a `HTTP` status code of `404` `Not Found`
+                // Send our validation object
+                res.status(404).json({ emailTaken: false });
+              // If a user was found with a matchin username
+              } else {
+                // Set a `HTTP` status code of `409` `Conflict`
+                // Send our validation object
+                res.status(409).json({ emailTaken: true });
               }
             }
           });
